df67c0b675154587972aa266ed9473c4047b7bd1
Usmanawaisu <95320088+Usmanawaisu@users.noreply.github.com>
2026-05-10 09:19:29 +0100

    Create NoteDao.kt

---
+++ NoteDao.kt
@@ -0,0 +1,23 @@
+package com.example.notesapp.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(note: Note)
+
+    @Update
+    suspend fun update(note: Note)
+
+    @Delete
+    suspend fun delete(note: Note)
+
+    @Query("SELECT * FROM notes_table ORDER BY id DESC")
+    fun getAllNotes(): LiveData<List<Note>>
+
+    @Query("SELECT * FROM notes_table WHERE title LIKE :searchQuery")
+    fun searchNotes(searchQuery: String): LiveData<List<Note>>
+}
